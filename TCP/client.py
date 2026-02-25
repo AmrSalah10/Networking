70dcab62601bd88bc4484c416f3235ac500373e8
AmrSalah10 <amr.thefirst2010@yahoo.com>
2026-02-26 00:12:55 +0200

    Add interactive TCP chat with predefined responses and conversation loop

--- TCP/client.py
+++ TCP/client.py
@@ -6,11 +6,22 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Connect to server
 client_socket.connect(("127.0.0.1", 8080))
 
-# Send data to server
-client_socket.send(b"Hello From Client")
-
-# Receive response from server
+# Receive any fixed msg from the server
 response = client_socket.recv(1024)
-print("Server:", response.decode())
+
+if response:
+    print("Server:", response.decode())
+
+while True:
+    # Send data to server
+    msg = input('Write Message: ')
+    client_socket.send(msg.encode())
+
+    # Receive response from server
+    response = client_socket.recv(1024)
+    print("Server:", response.decode())
+
+    if msg == 'Bye':
+        break
 
 client_socket.close()
--- TCP/server.py
+++ TCP/server.py
@@ -1,5 +1,10 @@
 import socket
 
+RESPONSES = {
+    "How are you?": "I am good",
+    "How is your day?": "Very good"
+}
+
 # Create socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -9,15 +14,34 @@ print("Server is Running on port 8080")
 
 server_socket.listen(5)
 
-# Accept client connection
-client_socket, addr = server_socket.accept()
+while True:
+    # Accept client connection
+    client_socket, addr = server_socket.accept()
+    port = addr[1]
 
-# Receive client msg
-msg = client_socket.recv(1024)
-print("Client", msg.decode())
+    print('Connection established with Client on Address: ', addr)
+    
+    # fixed message
+    client_socket.send(b"""
+        I am a simple server please choose your message:
+            1- How are you?
+            2- How is your day?
+            3- Bye (to close the connection)
+    """)
+    
+    # each client connection loop  
+    while True:
+        # Receive client msg
+        msg = client_socket.recv(1024).decode()
+        print("Client (%s):"%port, msg)
 
-# Send Response
-client_socket.send(b"Hello From Server")
+        # Close connection
+        if msg == 'Bye':
+            client_socket.send(b"See you next time")
+            client_socket.close()
+            break
 
-# Close connection
-client_socket.close()
+        # Send Response
+        client_socket.send(RESPONSES.get(msg, "I don't understand you").encode())
+
+    print('Connection closed with Client on Address: ', addr)
